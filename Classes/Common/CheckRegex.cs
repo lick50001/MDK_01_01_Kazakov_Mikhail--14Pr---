789a2965bf03e46279699655de5d84114452f319
Your Name <you@example.com>
2025-10-30 18:12:45 +0500

    Создание классов. Казаков

---
+++ Classes/Common/CheckRegex.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Regex_Kazakov.Classes.Common
+{
+    public class CheckRegex
+    {
+        public static bool Match(string Pattern, string Input){
+            Match m = Regex.Match(Input, Pattern);
+            return m.Success;
+        }
+    }
+}
